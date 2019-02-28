f262df949395d064a8548d65b6b8fb041ff10ee5
Süleyman <suleyman.altindag@student.hu.nl>
2019-02-28 15:17:03 +0100

    test 2 is misschien gelukt

---
+++ src/test/java/nl/hu/intergrationcummunication1/Test2.java
@@ -0,0 +1,23 @@
+package nl.hu.intergrationcummunication1;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class Test2 {
+
+    @Test
+    public void test2Succes() {
+        int a = 2;
+        int b = 10;
+        assertTrue(a + b == 12);
+    }
+
+    @Test
+    public void test2Fail() {
+        int a = 2;
+        int b = 10;
+        assertTrue(a + b == 12);
+
+    }
+}
